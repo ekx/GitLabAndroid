4d6c870674f35daa4ea52dd8a768bb29041d511f
steveyg <372125992@qq.com>
2016-08-09 20:05:54 +0800

    Delete Content.java
    del

--- app/src/main/java/com/bd/gitlab/util/Content.java
+++
@@ -1,9 +0,0 @@
-package com.bd.gitlab.util;
-
-/**
- * Created by yangguang on 2016/7/7.
- */
-public class Content {
-    public static final String HOST_URL = "http://gitlab.task.ac.cn/";
-//    public static final String USER_TOKEN = "sNCkfm4D_7sNmmLooefb";
-}
